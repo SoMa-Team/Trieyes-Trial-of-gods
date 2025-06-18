a7b9f1f54c864b3226b56953dfe617f5ab02e96b
SooinPark1019 <hyperion1019@naver.com>
2025-06-18 18:45:43 +0900

    중복 불가능 여부 버프 추가시 버프 리스트를 순회하는 로직 추가

--- Assets/Scripts/StatSystem/StatBuffList.cs
+++ Assets/Scripts/StatSystem/StatBuffList.cs
@@ -19,6 +19,9 @@ namespace Stats
         /// </summary>
         public void Add(StatBuff buff)
         {
+            if(!buff.canStack){
+                buffs.RemoveAll(b => b.key == buff.key);
+            }
             buffs.Add(buff);
         }
 
@@ -37,7 +40,7 @@ namespace Stats
         public int CalculateBuff(int basicValue)
         {
             // 만료된 버프 제거
-            float currentTime = CombatStageManager.Instance.GetTime();
+            float currentTime = BattleStageManager.Instance.GetTime();
             buffs.RemoveAll(buff => !buff.isPermanent && buff.endTime <= currentTime);
 
             int finalValue = basicValue;

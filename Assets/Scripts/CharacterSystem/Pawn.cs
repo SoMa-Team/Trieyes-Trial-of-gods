2812a26fc085fd23b08b77deb27d3da88c26e189
46891822+simigami@users.noreply.github.com <46891822+simigami@users.noreply.github.com>
2025-06-19 16:16:33 +0900

    feat: Pawn 부분의 이벤트 핸들러 추가

--- Assets/Scripts/CharacterSystem/Pawn.cs
+++ Assets/Scripts/CharacterSystem/Pawn.cs
@@ -24,6 +24,7 @@ namespace CharacterSystem
         public int level { get; protected set; }
         public int maxHp { get; protected set; }
         public int currentHp { get; protected set; }
+        public int gold { get; protected set; } // 골드 시스템 추가
 
         // ===== [기능 2] 스탯 시스템 =====
         protected Dictionary<StatType, StatInfo> stats = new();
@@ -78,6 +79,13 @@ namespace CharacterSystem
             {
                 statInfos.Add(new StatInfo(StatType.MoveSpeed, moveSpeed));
             }
+            if (!statInfos.Any(s => s.Type == StatType.Evasion))
+            {
+                statInfos.Add(new StatInfo(StatType.Evasion, 5)); // 기본 회피율 5%
+            }
+            
+            // 초기 골드 설정
+            gold = 0;
         }
         public virtual void Update() 
         {
@@ -86,7 +94,7 @@ namespace CharacterSystem
             {
                 isDead = true;
                 Debug.Log($"<color=red>{gameObject.name} has died. Triggering OnDeath event.</color>");
-                TriggerDeckActionInit(Utils.EventType.OnDeath, this);
+                TriggerEventActionInit(Utils.EventType.OnDeath, this);
                 ChangeAnimationState("Die"); 
                 if (rb != null) rb.bodyType = RigidbodyType2D.Static; 
                 if (boxCollider != null) boxCollider.enabled = false; 
@@ -103,14 +111,6 @@ namespace CharacterSystem
             if (direction.x != 0)
             {
                 ChangeAnimationState(WALK_ANIM);
-            }
-        }
-        public virtual void Jump()
-        {
-            if (isGrounded && rb != null)
-            {
-                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-                ChangeAnimationState(JUMP_ANIM);
             }
         }
         protected virtual void FixedUpdate()
@@ -200,9 +200,19 @@ namespace CharacterSystem
         }
 
         // ===== [기능 8] 덱 액션 트리거 =====
-        public void TriggerDeckActionInit(Utils.EventType eventType, object param = null)
+        public void TriggerEventActionInit(Utils.EventType eventType, object param = null)
         {
+            // 덱의 카드 액션들 처리
             deck.CalcActionInitStat(eventType, param);
+            
+            // 유물들의 이벤트 처리
+            foreach (var relic in relics)
+            {
+                if (relic != null)
+                {
+                    relic.OnEvent(eventType, param);
+                }
+            }
         }
 
         /// <summary>
@@ -214,22 +224,434 @@ namespace CharacterSystem
             // 기본 사망 처리 로직
         }
 
+        // ===== [기능 9] 이벤트 엔트리 포인트들 =====
+        
         /// <summary>
-        /// 전투 시작 이벤트 발생 시 호출되는 메서드입니다.
+        /// 전투 시작 이벤트를 발생시킵니다.
         /// </summary>
-        protected virtual void OnBattleStart(object param)
+        public void OnBattleStart()
         {
-            Debug.Log($"<color=green>{gameObject.name} (Pawn) battle started!</color>");
-            // 기본 전투 시작 처리 로직
+            Debug.Log($"<color=green>[EVENT] {gameObject.name} - OnBattleStart triggered</color>");
+            TriggerEventActionInit(Utils.EventType.OnBattleStart, this);
         }
 
         /// <summary>
-        /// 전투 종료 이벤트 발생 시 호출되는 메서드입니다.
+        /// 전투 클리어 이벤트를 발생시킵니다.
         /// </summary>
-        protected virtual void OnBattleEnd(object param)
+        public void OnBattleClear()
         {
-            Debug.Log($"<color=blue>{gameObject.name} (Pawn) battle ended!</color>");
-            // 기본 전투 종료 처리 로직
+            Debug.Log($"<color=blue>[EVENT] {gameObject.name} - OnBattleClear triggered</color>");
+            TriggerEventActionInit(Utils.EventType.OnBattleClear, this);
+        }
+
+        /// <summary>
+        /// OnTick 이벤트를 발생시킵니다. (30TPS로 호출 예정)
+        /// </summary>
+        public void OnTick()
+        {
+            // TODO: 30TPS로 호출되도록 구현 필요
+            TriggerEventActionInit(Utils.EventType.OnTick, this);
+        }
+
+        /// <summary>
+        /// 공격 성공 이벤트를 발생시킵니다.
+        /// </summary>
+        /// <param name="target">공격 대상</param>
+        public void OnAttack(Pawn target)
+        {
+            Debug.Log($"<color=yellow>[EVENT] {gameObject.name} - OnAttack triggered against {target.gameObject.name}</color>");
+            TriggerEventActionInit(Utils.EventType.OnAttack, new AttackEventData(this, target));
+        }
+
+        /// <summary>
+        /// 치명타 공격 성공 이벤트를 발생시킵니다.
+        /// </summary>
+        /// <param name="target">공격 대상</param>
+        public void OnCriticalAttack(Pawn target)
+        {
+            Debug.Log($"<color=red>[EVENT] {gameObject.name} - OnCriticalAttack triggered against {target.gameObject.name}</color>");
+            TriggerEventActionInit(Utils.EventType.OnCriticalAttack, new AttackEventData(this, target));
+        }
+
+        /// <summary>
+        /// 공격 투사체 명중 이벤트를 발생시킵니다.
+        /// </summary>
+        /// <param name="target">공격 대상</param>
+        public void OnAttackHit(Pawn target)
+        {
+            Debug.Log($"<color=orange>[EVENT] {gameObject.name} - OnAttackHit triggered against {target.gameObject.name}</color>");
+            TriggerEventActionInit(Utils.EventType.OnAttackHit, new AttackEventData(this, target));
+        }
+
+        /// <summary>
+        /// 공격이 회피당한 이벤트를 발생시킵니다.
+        /// </summary>
+        /// <param name="target">회피한 대상</param>
+        public void OnAttackMiss(Pawn target)
+        {
+            Debug.Log($"<color=cyan>[EVENT] {gameObject.name} - OnAttackMiss triggered against {target.gameObject.name}</color>");
+            TriggerEventActionInit(Utils.EventType.OnAttackMiss, new AttackEventData(this, target));
+        }
+
+        /// <summary>
+        /// 피격 이벤트를 발생시킵니다.
+        /// </summary>
+        /// <param name="attacker">공격자</param>
+        /// <param name="projectile">투사체 (선택사항)</param>
+        public void OnDamaged(Pawn attacker, Attack projectile = null)
+        {
+            Debug.Log($"<color=red>[EVENT] {gameObject.name} - OnDamaged triggered by {attacker.gameObject.name}</color>");
+            
+            // 이벤트 발생 (카드 액션이 스탯을 수정할 수 있도록)
+            TriggerEventActionInit(Utils.EventType.OnDamaged, new AttackEventData(attacker, this, projectile));
+            
+            // 실제 데미지 계산 및 적용 (이벤트 처리 후)
+            CalculateAndApplyDamage(attacker, projectile);
+        }
+
+        /// <summary>
+        /// 데미지를 계산하고 적용합니다.
+        /// </summary>
+        /// <param name="attacker">공격자</param>
+        /// <param name="projectile">투사체 (선택사항)</param>
+        protected virtual void CalculateAndApplyDamage(Pawn attacker, Attack projectile = null)
+        {
+            if (projectile == null || projectile.projectileStats == null)
+            {
+                Debug.LogError($"<color=red>[ERROR] {gameObject.name} - CalculateAndApplyDamage: projectile is null</color>");
+                return;
+            }
+
+            // 기본 공격력 가져오기
+            float attackPower = 10f;
+            var attackPowerStat = projectile.projectileStats.FirstOrDefault(s => s.Type == StatType.AttackPower);
+            if (attackPowerStat != null)
+            {
+                attackPower = attackPowerStat.Value;
+            }
+
+            // 치명타 확률과 치명타 데미지 가져오기
+            float criticalRate = 0f;
+            float criticalDamage = 1.5f; // 기본 치명타 배율 1.5배
+            
+            var criticalRateStat = projectile.projectileStats.FirstOrDefault(s => s.Type == StatType.CriticalRate);
+            if (criticalRateStat != null)
+            {
+                criticalRate = criticalRateStat.Value / 100f; // 퍼센트를 0~1 범위로 변환
+            }
+            
+            var criticalDamageStat = projectile.projectileStats.FirstOrDefault(s => s.Type == StatType.CriticalDamage);
+            if (criticalDamageStat != null)
+            {
+                criticalDamage = criticalDamageStat.Value / 100f; // 퍼센트를 배율로 변환
+            }
+
+            // 치명타 판정
+            bool isCritical = UnityEngine.Random.Range(0f, 1f) < criticalRate;
+            
+            // 방어력 계산
+            float defense = GetStatValue(StatType.Defense);
+            
+            // 최종 데미지 계산
+            float damageMultiplier = isCritical ? criticalDamage : 1f;
+            int finalDamage = Mathf.Max(1, Mathf.RoundToInt((attackPower - defense) * damageMultiplier));
+            
+            // HP 감소
+            ChangeHP(-finalDamage);
+            
+            // 데미지 로그 출력
+            string damageType = isCritical ? "CRITICAL" : "NORMAL";
+            Debug.Log($"<color=red>[DAMAGE] {gameObject.name} took {finalDamage} {damageType} damage from {attacker.gameObject.name} (Attack: {attackPower}, Defense: {defense}, Crit: {criticalRate * 100}%, CritDmg: {criticalDamage * 100}%)</color>");
+            
+            // 넉백 적용 (Rigidbody2D가 있는 경우)
+            ApplyKnockback(attacker);
+            
+            // 치명타 발생 시 공격자에게 OnCriticalAttack 이벤트 호출
+            if (isCritical)
+            {
+                attacker.OnCriticalAttack(this);
+            }
+        }
+
+        /// <summary>
+        /// 넉백을 적용합니다.
+        /// </summary>
+        /// <param name="attacker">공격자</param>
+        protected virtual void ApplyKnockback(Pawn attacker)
+        {
+            Rigidbody2D targetRb = GetComponent<Rigidbody2D>();
+            if (targetRb != null)
+            {
+                Vector2 knockbackDirection = (transform.position - attacker.transform.position).normalized;
+                float knockbackForce = 5f; // 기본 넉백 힘
+                targetRb.AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
+                
+                Debug.Log($"<color=orange>[KNOCKBACK] {gameObject.name} knocked back by {attacker.gameObject.name}</color>");
+            }
+        }
+
+        /// <summary>
+        /// 피격 투사체 명중 이벤트를 발생시킵니다.
+        /// </summary>
+        /// <param name="attacker">공격자</param>
+        public void OnDamageHit(Pawn attacker)
+        {
+            Debug.Log($"<color=orange>[EVENT] {gameObject.name} - OnDamageHit triggered by {attacker.gameObject.name}</color>");
+            
+            // 회피 판정 (스탯 기반)
+            float evasionRate = GetStatValue(StatType.Evasion) / 100f; // 회피율을 0~1 범위로 변환
+            bool isEvaded = UnityEngine.Random.Range(0f, 1f) < evasionRate;
+            
+            if (isEvaded)
+            {
+                Debug.Log($"<color=cyan>[EVENT] {gameObject.name} - OnEvaded triggered (Evasion Rate: {evasionRate * 100}%)</color>");
+                TriggerEventActionInit(Utils.EventType.OnEvaded, this);
+            }
+            else
+            {
+                TriggerEventActionInit(Utils.EventType.OnDamageHit, new AttackEventData(attacker, this));
+            }
+        }
+
+        /// <summary>
+        /// 회피 성공 이벤트를 발생시킵니다.
+        /// </summary>
+        public void OnEvaded()
+        {
+            Debug.Log($"<color=cyan>[EVENT] {gameObject.name} - OnEvaded triggered</color>");
+            TriggerEventActionInit(Utils.EventType.OnEvaded, this);
+        }
+
+        /// <summary>
+        /// 적 처치 이벤트를 발생시킵니다.
+        /// </summary>
+        /// <param name="target">처치된 적</param>
+        public void OnKilled(Pawn target)
+        {
+            Debug.Log($"<color=purple>[EVENT] {gameObject.name} - OnKilled triggered against {target.gameObject.name}</color>");
+            TriggerEventActionInit(Utils.EventType.OnKilled, new AttackEventData(this, target));
+        }
+
+        /// <summary>
+        /// 크리티컬 공격으로 적 처치 이벤트를 발생시킵니다.
+        /// </summary>
+        /// <param name="target">처치된 적</param>
+        public void OnKilledByCritical(Pawn target)
+        {
+            Debug.Log($"<color=purple>[EVENT] {gameObject.name} - OnKilledByCritical triggered against {target.gameObject.name}</color>");
+            TriggerEventActionInit(Utils.EventType.OnKilledByCritical, new AttackEventData(this, target));
+        }
+
+        /// <summary>
+        /// 스킬 쿨타임 완료 이벤트를 발생시킵니다.
+        /// </summary>
+        /// <param name="skill">쿨타임이 완료된 스킬</param>
+        public void OnSkillCooldownEnd(Attack skill)
+        {
+            Debug.Log($"<color=green>[EVENT] {gameObject.name} - OnSkillCooldownEnd triggered for skill</color>");
+            TriggerEventActionInit(Utils.EventType.OnSkillCooldownEnd, new SkillEventData(this, skill));
+        }
+
+        /// <summary>
+        /// 스킬 입력 이벤트를 발생시킵니다.
+        /// </summary>
+        /// <param name="skill">입력된 스킬</param>
+        public void OnSkillInput(Attack skill)
+        {
+            Debug.Log($"<color=blue>[EVENT] {gameObject.name} - OnSkillInput triggered for skill</color>");
+            TriggerEventActionInit(Utils.EventType.OnSkillInput, new SkillEventData(this, skill));
+        }
+
+        /// <summary>
+        /// HP 업데이트 이벤트를 발생시킵니다.
+        /// </summary>
+        /// <param name="preHP">이전 HP 값</param>
+        public void OnHPUpdated(int preHP)
+        {
+            Debug.Log($"<color=green>[EVENT] {gameObject.name} - OnHPUpdated triggered (PreHP: {preHP}, CurrentHP: {currentHp})</color>");
+            TriggerEventActionInit(Utils.EventType.OnHPUpdated, new StatUpdateEventData(this, preHP));
+        }
+
+        /// <summary>
+        /// 골드 업데이트 이벤트를 발생시킵니다.
+        /// </summary>
+        /// <param name="preGold">이전 골드 값</param>
+        public void OnGoldUpdated(int preGold)
+        {
+            Debug.Log($"<color=yellow>[EVENT] {gameObject.name} - OnGoldUpdated triggered (PreGold: {preGold})</color>");
+            TriggerEventActionInit(Utils.EventType.OnGoldUpdated, new StatUpdateEventData(this, preGold));
+        }
+
+        // ===== [기능 10] 이벤트 데이터 클래스들 =====
+        
+        /// <summary>
+        /// 공격 관련 이벤트 데이터
+        /// </summary>
+        public class AttackEventData
+        {
+            public Pawn attacker;
+            public Pawn target;
+            public Attack projectile; // 투사체 정보 (데미지 계산용)
+            
+            public AttackEventData(Pawn attacker, Pawn target)
+            {
+                this.attacker = attacker;
+                this.target = target;
+                this.projectile = null;
+            }
+            
+            public AttackEventData(Pawn attacker, Pawn target, Attack projectile)
+            {
+                this.attacker = attacker;
+                this.target = target;
+                this.projectile = projectile;
+            }
+        }
+
+        /// <summary>
+        /// 스킬 관련 이벤트 데이터
+        /// </summary>
+        public class SkillEventData
+        {
+            public Pawn owner;
+            public Attack skill;
+            
+            public SkillEventData(Pawn owner, Attack skill)
+            {
+                this.owner = owner;
+                this.skill = skill;
+            }
+        }
+
+        /// <summary>
+        /// 스탯 업데이트 관련 이벤트 데이터
+        /// </summary>
+        public class StatUpdateEventData
+        {
+            public Pawn owner;
+            public int previousValue;
+            
+            public StatUpdateEventData(Pawn owner, int previousValue)
+            {
+                this.owner = owner;
+                this.previousValue = previousValue;
+            }
+        }
+
+        // ===== [기능 8] HP 및 골드 관리 =====
+        
+        /// <summary>
+        /// HP를 변경하고 이벤트를 발생시킵니다.
+        /// </summary>
+        /// <param name="amount">변경할 HP 양 (음수면 감소, 양수면 증가)</param>
+        public void ChangeHP(int amount)
+        {
+            int preHP = currentHp;
+            currentHp = Mathf.Clamp(currentHp + amount, 0, maxHp);
+            
+            if (preHP != currentHp)
+            {
+                OnHPUpdated(preHP);
+            }
+        }
+
+        /// <summary>
+        /// 골드를 변경하고 이벤트를 발생시킵니다.
+        /// </summary>
+        /// <param name="amount">변경할 골드 양 (음수면 감소, 양수면 증가)</param>
+        public void ChangeGold(int amount)
+        {
+            int preGold = gold;
+            gold = Mathf.Max(0, gold + amount);
+            
+            if (preGold != gold)
+            {
+                OnGoldUpdated(preGold);
+            }
+        }
+
+        /// <summary>
+        /// 골드를 획득합니다 (적 처치 시 호출).
+        /// </summary>
+        /// <param name="amount">획득할 골드 양</param>
+        public void EarnGold(int amount)
+        {
+            ChangeGold(amount);
+        }
+
+        // ===== [기능 9] 유물 관리 =====
+        
+        /// <summary>
+        /// 유물을 추가합니다.
+        /// </summary>
+        /// <param name="relic">추가할 유물</param>
+        public void AddRelic(Relic relic)
+        {
+            if (relic != null)
+            {
+                relic.SetOwner(this);
+                relics.Add(relic);
+                Debug.Log($"<color=purple>[RELIC] {gameObject.name} acquired relic: {relic.info.name}</color>");
+            }
+        }
+
+        /// <summary>
+        /// 유물을 제거합니다.
+        /// </summary>
+        /// <param name="relic">제거할 유물</param>
+        public void RemoveRelic(Relic relic)
+        {
+            if (relic != null && relics.Contains(relic))
+            {
+                relics.Remove(relic);
+                Debug.Log($"<color=purple>[RELIC] {gameObject.name} lost relic: {relic.info.name}</color>");
+            }
+        }
+
+        // ===== [기능 6] 투사체 발사 =====
+        /// <summary>
+        /// 공격을 발사합니다. (모든 공격의 통합 엔트리 포인트)
+        /// </summary>
+        /// <param name="attackPrefab">발사할 Attack 프리팹</param>
+        /// <param name="direction">발사 방향</param>
+        /// <param name="parentAttack">부모 Attack (선택사항)</param>
+        public virtual void FireAttack(Attack attackPrefab, Vector2 direction, Attack parentAttack = null)
+        {
+            if (attackPrefab == null)
+            {
+                Debug.LogWarning($"<color=red>[PROJECTILE] {gameObject.name} - attackPrefab is null</color>");
+                return;
+            }
+
+            // 1. 연산을 통해 투사체가 가져야하는 스탯 정보를 갱신
+            // (이미 Pawn의 현재 스탯이 최신 상태이므로 별도 갱신 불필요)
+
+            // 2. 투사체를 몇개, 그리고 어떤 효과를 가지는 Comp를 몇개 만들어야 하는지 알아옴
+            // (현재는 단일 투사체, 향후 확장 가능)
+
+            // 3. Attack 객체를 만들고 쏨
+            Attack projectile = Instantiate(attackPrefab, transform.position, Quaternion.identity);
+            
+            // 투사체 초기화
+            projectile.InitializeProjectile(this, direction, parentAttack);
+            
+            Debug.Log($"<color=green>[PROJECTILE] {gameObject.name} fired attack {projectile.gameObject.name}</color>");
+        }
+
+        /// <summary>
+        /// 기본 공격을 발사합니다.
+        /// </summary>
+        /// <param name="direction">발사 방향</param>
+        public virtual void FireBasicAttack(Vector2 direction)
+        {
+            if (basicAttack != null)
+            {
+                FireAttack(basicAttack, direction);
+            }
+            else
+            {
+                Debug.LogWarning($"<color=red>[PROJECTILE] {gameObject.name} - basicAttack is null</color>");
+            }
         }
     }
 } 

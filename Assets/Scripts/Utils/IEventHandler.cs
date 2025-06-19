22a8b81bed23dc4daad18b4608374f5e65b32784
46891822+simigami@users.noreply.github.com <46891822+simigami@users.noreply.github.com>
2025-06-20 00:16:31 +0900

    add: 이벤트 핸들러에 Deck에서 카드 호출 순서를 구하는 전용 이벤트 핸들러 생성

--- Assets/Scripts/Utils/IEventHandler.cs
+++ Assets/Scripts/Utils/IEventHandler.cs
@@ -16,6 +16,7 @@ namespace Utils
         OnBattleClear,        // 전투 라운드가 클리어되는 시점에 호출
         OnBattleEnd,          // 전투 종료 이벤트
         OnBattleSceneChange,  // 전투 씬 생성 이전 -> 생성하려고 하는 즉시 호출되는 이벤트
+        CalcActionInitOrder,  // 카드 액션 호출 순서 계산 이벤트
         
         // ===== 상시 호출 관련 =====
         OnTick,               // 전투 시작 후, 30TPS로 호출 (주기적인 감시가 필요한 데이터/동작에 사용)

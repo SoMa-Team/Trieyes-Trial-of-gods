e4245d693d95909e87ced2063547e06f8e219ef2
46891822+simigami@users.noreply.github.com <46891822+simigami@users.noreply.github.com>
2025-06-19 14:19:37 +0900

    feat: Attack 계열 이벤트 핸들링 코드 추가

--- Assets/Scripts/AttackSystem/Attack.cs
+++ Assets/Scripts/AttackSystem/Attack.cs
@@ -10,7 +10,7 @@ namespace AttackSystem
     /// 게임 내 공격 행위를 정의하는 클래스입니다.
     /// 이 클래스는 IEventHandler를 구현하여 자체적으로 이벤트를 처리하고 발동시킬 수 있습니다.
     /// </summary>
-    public class Attack : IEventHandler
+    public abstract class Attack : IEventHandler
     {
         // ===== 생성자 및 공용 =====
         public Attack(AttackData data)
@@ -43,23 +43,12 @@ namespace AttackSystem
         }
         
         // ===== [기능 2] 이벤트 처리 =====
-        public virtual void OnEvent(Utils.EventType eventType, object param)
+        public abstract void OnEvent(Utils.EventType eventType, object param);
+
+        // ===== [기능 3] 공격 실행 =====
+        public virtual void Execute(Pawn target)
         {
-            switch (eventType)
-            {
-                case Utils.EventType.OnBattleEnd:
-                Debug.Log($"Attack: 전투 종료 이벤트 수신! 공격 비활성화.");
-                Deactivate();
-                    break;
-                case Utils.EventType.OnDeath:
-                if (param is CharacterSystem.Pawn deadPawn)
-                {
-                    Debug.Log($"Attack: {deadPawn.gameObject.name} 사망 이벤트 수신. 특정 공격 버프 제거.");
-                    // 사망한 대상에 따른 공격 관련 로직 구현
-                }
-                    break;
-            // 다른 이벤트 처리 로직 추가
-        }
+            // 공격 실행 로직
         }
     }
 } 

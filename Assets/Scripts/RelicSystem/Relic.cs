33335478b657fbdb10b4a5fe3ed3adc0933781e4
46891822+simigami@users.noreply.github.com <46891822+simigami@users.noreply.github.com>
2025-06-19 14:18:21 +0900

    feat: Relic 이벤트 핸들러 준비

--- Assets/Scripts/RelicSystem/Relic.cs
+++ Assets/Scripts/RelicSystem/Relic.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Utils;
+using UnityEngine;
 
 namespace RelicSystem
 {
@@ -7,7 +8,7 @@ namespace RelicSystem
     /// 게임 내 유물을 나타내는 클래스입니다.
     /// 유물은 자체적으로 이벤트를 등록하고 처리할 수 있는 IEventHandler를 구현합니다.
     /// </summary>
-    public class Relic : IEventHandler
+    public abstract class Relic : IEventHandler
     {
         // ===== [기능 1] 유물 정보 및 생성 =====
         public RelicInfo info;
@@ -17,19 +18,6 @@ namespace RelicSystem
         }
 
         // ===== [기능 2] 이벤트 처리 =====
-        public virtual void OnEvent(EventType eventType, object param)
-        {
-            switch (eventType)
-            {
-                // 예시: 유물 관련 이벤트별 동작 하드코딩
-                case EventType.OnLevelUp:
-                    // 레벨업 시 효과
-                    break;
-                case EventType.OnStatChange:
-                    // 스탯 변경 시 효과
-                    break;
-                // 기타 이벤트별 동작 추가
-            }
-        }
+        public abstract void OnEvent(Utils.EventType eventType, object param);
     }
 } 

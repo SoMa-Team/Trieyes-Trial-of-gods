3778384d290d010db68c1ceab4f054c55160ed0c
SooinPark1019 <hyperion1019@naver.com>
2025-06-23 21:53:21 +0900

    feat: card factory 작성

--- Assets/Scripts/CardSystem/CardFactory.cs
+++ Assets/Scripts/CardSystem/CardFactory.cs
@@ -7,28 +7,33 @@ namespace CardSystem
 {
     public class CardFactory : MonoBehaviour
     {
-        [Header("Card Prefabs")]
-        [SerializeField] private List<Card> cardPrefabs = new List<Card>();
+        public static CardFactory Instance { private set; get; }
 
-        /// <summary>
-        /// 주어진 카드 ID에 해당하는 Card 인스턴스를 생성하여 반환합니다.
-        /// </summary>
-        /// <param name="cardId">생성할 카드의 고유 ID</param>
-        /// <returns>생성된 Card 인스턴스</returns>
-        public Card GetCard(int cardId)
+        private void Awake()
         {
-            Card prefab = cardPrefabs.FirstOrDefault(p => p.cardId == cardId);
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+        }
 
-            if (prefab != null)
-            {
-                // 프리팹을 복제하여 새 인스턴스를 만듭니다.
-                return Instantiate(prefab);
-            }
-            else
-            {
-                Debug.LogWarning($"Card prefab with ID {cardId} not found in factory.");
-                return null;
-            }
+        public Card Create(int level, int CardActionID)
+        {
+            Card card = new Card();
+            Activate(card, level, CardActionID);
+            return card;
+        }
+
+        public void Activate(Card card, int level, int CardActionID)
+        {
+            card.Activate(level, CardActionID);
+        }
+
+        public void Deactivate(Card card)
+        {
+            card.Deactivate();
         }
     }
 } 

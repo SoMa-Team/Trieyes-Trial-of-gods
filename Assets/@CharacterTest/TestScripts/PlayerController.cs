3f5f11c2e08ee979d5517cb7ec265a3569e0d316
46891822+simigami@users.noreply.github.com <46891822+simigami@users.noreply.github.com>
2025-06-27 18:51:55 +0900

    modify: Input Action을 이용한 입력 시스템 변경

--- Assets/@CharacterTest/TestScripts/PlayerController.cs
+++ Assets/@CharacterTest/TestScripts/PlayerController.cs
@@ -6,12 +6,17 @@ namespace CharacterSystem
     public class PlayerController : Controller
     {
         public InputActionReference moveAction; // 에디터에서 할당
+        public InputActionReference attackAction; // 에디터에서 할당
 
         private void OnEnable()
         {
             if (moveAction != null)
             {
                 moveAction.action.Enable();
+            }
+            if (attackAction != null)
+            {
+                attackAction.action.Enable();
             }
         }
 
@@ -25,14 +30,24 @@ namespace CharacterSystem
 
         public override void ProcessInput() // To-Do: 이름 변경
         {
+            if (owner == null)
+            {
+                return;
+            }
+
             Vector2 moveDir = Vector2.zero;
             if (moveAction != null)
             {
                 moveDir = moveAction.action.ReadValue<Vector2>();
-            }
-            if (owner != null)
-            {
                 owner.Move(moveDir);
+            }
+
+            if (attackAction != null)
+            {
+                if (attackAction.action.ReadValue<float>() > 0)
+                {
+                    owner.PerformAutoAttack();
+                }
             }
         }
     }

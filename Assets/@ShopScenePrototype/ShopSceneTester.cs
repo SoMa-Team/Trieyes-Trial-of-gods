0b27f51e96ff650695b3830b0f8a09e3a9a2b1b1
SooinPark1019 <hyperion1019@naver.com>
2025-07-01 21:44:42 +0900

    feat: 상점에 카드 랜덤 생성, 리롤 로직까지 구현 완료

--- Assets/@ShopScenePrototype/ShopSceneTester.cs
+++ Assets/@ShopScenePrototype/ShopSceneTester.cs
@@ -1,20 +1,37 @@
 using UnityEngine;
 using CardViews;
+using UnityEngine.UI;
+using CardSystem;
 
 public class ShopSceneTester : MonoBehaviour
 {
     public CardView cardView1;
     public CardView cardView2;
     public CardView cardView3;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public Button BuyButton1;
+    public Button BuyButton2;
+    public Button BuyButton3;
+
+    public Button RerollButton;
+    
+    public CardFactory cardFactory;
+
+    private void Awake()
     {
+        Reroll();
         
+        RerollButton.onClick.AddListener(Reroll);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Reroll()
     {
+        Card card1 = cardFactory.Create(UnityEngine.Random.Range(1, 4), UnityEngine.Random.Range(0, cardFactory.cardInfos.Count));
+        Card card2 = cardFactory.Create(UnityEngine.Random.Range(1, 4), UnityEngine.Random.Range(0, cardFactory.cardInfos.Count));
+        Card card3 = cardFactory.Create(UnityEngine.Random.Range(1, 4), UnityEngine.Random.Range(0, cardFactory.cardInfos.Count));
         
+        cardView1.SetCard(card1);
+        cardView2.SetCard(card2);
+        cardView3.SetCard(card3);
     }
 }

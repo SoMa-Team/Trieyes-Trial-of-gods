063beb8fc8baf173a93e1402f1f0e99ced744b8d
SooinPark1019 <hyperion1019@naver.com>
2025-07-13 16:03:10 +0900

    feat: Sticker 클래스 기본 작성

---
+++ Assets/Trieyes/Scripts/ItemSystem/StickerSystem/Sticker.cs
@@ -0,0 +1,30 @@
+using Stats;
+
+namespace StickerSystem
+{
+    public enum StickerType
+    {
+        Number,
+        StatType
+    }
+
+    public class Sticker
+    {
+        public StickerType type;
+        public int numberValue;          // type==Number일 때만 사용
+        public StatType statTypeValue;   // type==StatType일 때만 사용
+
+        // 생성자: StickerType/범위 등 인자로 직접 만듦
+        public Sticker(int value) // 숫자 스티커
+        {
+            type = StickerType.Number;
+            numberValue = value;
+        }
+        public Sticker(StatType statType) // 스탯 스티커
+        {
+            type = StickerType.StatType;
+            statTypeValue = statType;
+        }
+    }
+
+}

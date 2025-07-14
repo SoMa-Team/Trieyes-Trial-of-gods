2461e544cb001828d53dcada45b023c3c437016c
SooinPark1019 <hyperion1019@naver.com>
2025-07-14 20:27:33 +0900

    feat: sticker view 작성, 프리팹 생성

--- Assets/Trieyes/Scripts/ItemSystem/StickerSystem/Sticker.cs
+++ Assets/Trieyes/Scripts/ItemSystem/StickerSystem/Sticker.cs
@@ -16,15 +16,17 @@ namespace StickerSystem
         public int lifeTime;
 
         // 생성자: StickerType/범위 등 인자로 직접 만듦
-        public Sticker(int value) // 숫자 스티커
+        public Sticker(int value, int life) // 숫자 스티커
         {
             type = StickerType.Number;
             numberValue = value;
+            lifeTime = life;
         }
-        public Sticker(StatType statType) // 스탯 스티커
+        public Sticker(StatType statType, int life) // 스탯 스티커
         {
             type = StickerType.StatType;
             statTypeValue = statType;
+            lifeTime = life;
         }
     }
 

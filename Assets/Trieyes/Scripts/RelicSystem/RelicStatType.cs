1d97d39376b08b397cef68e995ae7732251ec8a8
delena0702 <delena0702@gmail.com>
2025-07-17 18:26:30 +0900

    feat: RelicStat 예시 추가

--- Assets/Trieyes/Scripts/RelicSystem/RelicStatType.cs
+++ Assets/Trieyes/Scripts/RelicSystem/RelicStatType.cs
@@ -2,9 +2,14 @@ namespace RelicSystem
 {
     public enum RelicStatType
     {
-        DamageIncreasement,
-        AOE,
-        Range,
-        ProjectileCount
+        DamageIncreasement, // 최종 데미지 증가
+        AOE,                // 광역 공격 범위
+        Range,              // 투사체 범위
+        ProjectileCount,    // 투사체 개수
+        ProjectilePierce,   // 투사체 관통 개수
+        ProjectileSpeed,    // 투사체 속도
+        ProjectileSpread,   // 투사체 분산각도
+        SkillCooldownReduction, // 스킬 쿨타임 감소
+        Revival,            // 부활
     }
 }

32ff4d5ea2a1def77f915fac1f111ff7ece36a15
SooinPark1019 <hyperion1019@naver.com>
2025-07-31 19:25:55 +0900

    fix: 버그 해결

--- Assets/Trieyes/Scripts/Utils/Difficulty.cs
+++ Assets/Trieyes/Scripts/Utils/Difficulty.cs
@@ -7,7 +7,7 @@ namespace Utils
         // ===== [기능 1] 난이도 정보 =====
         public int spawnFrequency => 1;
         public float enemyStatMultiplier = 1.0f;
-        public float battleLength => 30; // 초단위
+        public float battleLength => 90; // 초단위
         public int EnemyID = 0;
         public int shopLevel = 1;
         public int stageNumber => 12;

a7e89099e1b677fce02d1baf87a65c19d638097c
SooinPark1019 <hyperion1019@naver.com>
2025-07-31 19:13:24 +0900

    feat: GameOverManager 작성

--- Assets/Trieyes/Scripts/GameOver/GameOverManager.cs
+++ Assets/Trieyes/Scripts/GameOver/GameOverManager.cs
@@ -1,9 +1,25 @@
+using System;
+using GameFramework;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Trieyes.Scripts.GameOver
 {
     public class GameOverManager : MonoBehaviour
     {
-        
+        public Button RetryButton;
+
+        private void Awake()
+        {
+            if (RetryButton != null)
+            {
+                RetryButton.onClick.AddListener(OnRetryButtonPressed);
+            }
+        }
+
+        private void OnRetryButtonPressed()
+        {
+            SceneChangeManager.Instance.ChangeGameOverToGameStart();
+        }
     }
 }

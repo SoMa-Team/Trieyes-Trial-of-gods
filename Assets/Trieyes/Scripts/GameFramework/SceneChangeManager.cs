cfb7006e849a00f86b78a0f8feadadce41283694
46891822+simigami@users.noreply.github.com <46891822+simigami@users.noreply.github.com>
2025-08-04 02:27:30 +0900

    add: AC002 화염 도트 데미지 구현 중

--- Assets/Trieyes/Scripts/GameFramework/SceneChangeManager.cs
+++ Assets/Trieyes/Scripts/GameFramework/SceneChangeManager.cs
@@ -110,7 +110,7 @@ namespace GameFramework
             var mainCharacter = CharacterFactory.Instance.Create(0);
 
 
-            mainCharacter.AddRelic(RelicFactory.Create(720011));
+            // mainCharacter.AddRelic(RelicFactory.Create(720011));
             // mainCharacter.AddRelic(RelicFactory.Create(720011));
             // mainCharacter.AddRelic(RelicFactory.Create(720013));
             mainCharacter.ApplyRelic();

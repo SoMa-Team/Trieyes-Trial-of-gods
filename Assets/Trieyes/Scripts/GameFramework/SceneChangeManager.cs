6e1531138c0ff8c209d6620a9e213d815351c6cc
46891822+simigami@users.noreply.github.com <46891822+simigami@users.noreply.github.com>
2025-08-08 18:27:23 +0900

    no message

--- Assets/Trieyes/Scripts/GameFramework/SceneChangeManager.cs
+++ Assets/Trieyes/Scripts/GameFramework/SceneChangeManager.cs
@@ -109,7 +109,7 @@ namespace GameFramework
         {
             var mainCharacter = CharacterFactory.Instance.Create(0);
 
-            // mainCharacter.AddRelic(RelicFactory.Create(720014));
+            mainCharacter.AddRelic(RelicFactory.Create(720014));
             mainCharacter.ApplyRelic();
 
             CharacterFactory.Instance.Deactivate(mainCharacter);
